a9097daf8ce4efee66a88cc4f6140c5da4cc06f3
Ben Sigman <bensig@gmail.com>
2023-08-04 15:45:48 -0700

    add files

---
+++ generate_tweets_json.py
@@ -0,0 +1,33 @@
+import csv
+import json
+import re
+import sys
+
+def extract_tweet_id(url):
+    # Extract tweet ID from the URL using regular expression
+    return re.search(r'/status/(\d+)', url).group(1)
+
+def csv_to_json(input_file):
+    # Read data from CSV file and create a dictionary with token as key and tweet IDs as values
+    data = {}
+    with open(input_file, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 2:
+                token, url = row
+                tweet_id = extract_tweet_id(url)
+                data.setdefault(token, []).append(tweet_id)
+
+    # Convert the dictionary to JSON
+    json_data = json.dumps(data, indent=2)
+
+    # Save the JSON data to "tweets.json"
+    with open('tweets.json', 'w') as jsonfile:
+        jsonfile.write(json_data)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py data.csv")
+    else:
+        input_file = sys.argv[1]
+        csv_to_json(input_file)
